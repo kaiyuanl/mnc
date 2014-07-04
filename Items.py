81179b875c3e59d2873c08dd9ff4ec187204d898
kaiyuanl <liangkaiyuan@hotmail.com>
2014-07-04 19:35:04 +0800

    commit file items.py

---
+++ Items.py
@@ -0,0 +1,22 @@
+class Issue(object):
+	def __init__(self, number, title, year, month, day, url):
+		self.number = number
+		self.title = title
+		self.year = year
+		self.month = month
+		self.day = day
+		self.url = url
+
+
+class Item(object):
+	def __init__(self, number, category, title, titleUrl, subtitle, subtitleUrl):
+		self.number
+		self.category = category
+		self.title = title;
+		self.titleUrl = titleUrl
+		self.subtitle = subtitle
+		self.subtitleUrl = subtitleUrl
+
+
+
+
