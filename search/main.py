6dfc88e324e6f9b414fd7072c0c09d399740d721
kaiyuanl <liangkaiyuan@hotmail.com>
2014-07-30 20:08:04 +0800

    add main file as a startup script

--- search/main.py
+++ search/main.py
@@ -1,15 +1,36 @@
-import spiders
-import data
-import helper
+from spider import *
+from helper import *
 from globalvs import *
 
-manongUrl = ''
-issueSpider = spiders.IssueSpider(manongUrl)
-issues = issueSpider.GetNewIssues()
+curr_date = get_current_date()
+last_update_date = get_daily_last_update_date()
+dates = get_days_between_dates(last_update_date, curr_date)
 
-connString = helper.GetDatabaseConnectionString()
-conn = data.Connector(connString)
+for date_ in dates:
+    year = date_.year
+    month = date_.month
+    day = date_.day
 
-for issue in issues:
-	conn.PushIssueToDatabase(issue)
+    daily_url = gen_daily_url(year, month, day)
+    if(test_url_valid(daily_url)):
+        spider = DailyContentSpider(daily_url)
+        spider.fill_items()
+        posts = spider.get_posts()
+        for post in posts:
+            push_daily_post()
 
+
+
+last_issue = get_last_issue()
+new_issue = last_issue + 1
+weekly_url = gen_weekly_url(new_issue)
+if(test_url_valid(weekly_url)):
+    spider = IssueContentSpider(weekly_url)
+    spider.fill_items()
+    posts = spider.get_posts()
+    jobs = spider.get_jobs()
+    for post in posts:
+        push_weekly_post(post)
+
+    for job in jobs:
+        push_job(job)
