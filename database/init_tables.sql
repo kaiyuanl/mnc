bc7ab67eb229e5043152883a5b5230add0791078
kaiyuanl <liangkaiyuan@hotmail.com>
2014-07-31 20:37:03 +0800

    add constraint foreign key

--- database/init_tables.sql
+++ database/init_tables.sql
@@ -1,5 +1,3 @@
-
-
 CREATE DATABASE IF NOT EXISTS manong;
 
 DROP TABLE IF EXISTS Issue;
@@ -8,18 +6,30 @@ CREATE TABLE Issue
 (
     Num int not null,
     Title varchar(200) not null,
-    Url varchar(200) not null,
+    PubDate date not null,
     PRIMARY KEY(Num)
 );
 
-DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS WeeklyPost;
 
-CREATE TABLE Post
+CREATE TABLE WeeklyPost
 (
     ID int not null AUTO_INCREMENT,
     Issue int not null,
     Title varchar(200) not null,
     Description varchar(200) not null,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(Issue) REFERENCES Issue(Num)
+);
+
+DROP TABLE IF EXISTS DailyPost;
+
+CREATE TABLE DailyPost
+(
+    ID in not null AUTO_INCREMENT,
+    Title varchar(200) not null,
+    Src varchar(200) not null,
+    PubDate date not null,
     PRIMARY KEY(ID)
 );
 
@@ -28,7 +38,9 @@ DROP TABLE IF EXISTS Job;
 CREATE TABLE Job
 (
     ID int not null AUTO_INCREMENT,
+    Issue int not null,
     Company varchar(200) not null,
     Position varchar(200) not null,
-    PRIMARY KEY(ID)
+    PRIMARY KEY(ID),
+    FOREIGN KEY(Issue) REFERENCES Issue(Num)
 );
