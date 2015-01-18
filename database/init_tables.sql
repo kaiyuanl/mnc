862c976fed5ae3a731ad43386621ee171a17fab5
Kaiyuan Liang <liangkaiyuan@hotmail.com>
2015-01-18 13:33:35 +0800

    change to sql script

--- database/init_tables.sql
+++ database/init_tables.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS manong;
+USE manong;
 
 DROP TABLE IF EXISTS Issue;
 
@@ -17,30 +18,36 @@ CREATE TABLE WeeklyPost
     ID MEDIUMINT not null AUTO_INCREMENT,
     Issue int not null,
     Title varchar(200) not null,
-    Description varchar(200) not null,
+    Origin varchar(200) null,
+    Link varchar(300) null,
     PRIMARY KEY(ID),
     FOREIGN KEY(Issue) REFERENCES Issue(Num)
 );
 
-DROP TABLE IF EXISTS DailyPost;
 
-CREATE TABLE DailyPost
-(
-    ID int not null AUTO_INCREMENT,
-    Title varchar(200) not null,
-    Src varchar(200) not null,
-    PubDate date not null,
-    PRIMARY KEY(ID)
-);
+DROP PROCEDURE IF EXISTS GetLastIssue;
+DELIMITER $$
+CREATE PROCEDURE GetLastIssue(OUT issue_no INT)
+BEGIN
+    select MAX(Num) into issue_no
+    from manong.Issue;
+END$$
+DELIMITER ;
 
-DROP TABLE IF EXISTS Job;
 
-CREATE TABLE Job
-(
-    ID int not null AUTO_INCREMENT,
-    Issue int not null,
-    Company varchar(200) not null,
-    Position varchar(200) not null,
-    PRIMARY KEY(ID),
-    FOREIGN KEY(Issue) REFERENCES Issue(Num)
-);
+drop procedure if exists PushWeeklyPost;
+delimiter $$
+create procedure PushWeeklyPost(IN issue_no int, IN title varchar(200), IN Origin varchar(200), IN Link varchar(300))
+begin
+    insert into manong.WeeklyPost(Issue, Title, Origin, Link) value(issue_no, title, Origin, Link);
+end$$
+delimiter ;
+
+
+drop procedure if exists PushNewIssue;
+delimiter $$
+create procedure PushNewIssue(IN issue_no int, IN title varchar(200), IN pub_date date)
+begin
+    insert into manong.Issue value(issue_no, title, pub_date);
+end$$
+delimiter ;
